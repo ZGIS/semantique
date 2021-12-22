56c45a7dac16ffd7501f6c5d908554efcc94e9af
Luuk van der Meer <luukvandermeer@live.nl>
2021-12-22 12:28:00 +0100

    fix: Call promote_value_type as sq method in reducers :wrench:

--- semantique/processor/reducers.py
+++ semantique/processor/reducers.py
@@ -19,7 +19,7 @@ def mean_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["numerical_reducers"]
-    out.promote_value_type(x, func = "mean", manual = manual)
+    out.sq.promote_value_type(x, func = "mean", manual = manual)
   return out
 
 def product_(x, dimension, track_types = False, **kwargs):
@@ -29,7 +29,7 @@ def product_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["numerical_reducers"]
-    out.promote_value_type(x, func = "product", manual = manual)
+    out.sq.promote_value_type(x, func = "product", manual = manual)
   return out
 
 def standard_deviation_(x, dimension, track_types = False, **kwargs):
@@ -38,7 +38,7 @@ def standard_deviation_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["numerical_reducers"]
-    out.promote_value_type(x, func = "standard_deviation", manual = manual)
+    out.sq.promote_value_type(x, func = "standard_deviation", manual = manual)
   return out
 
 def sum_(x, dimension, track_types = False, **kwargs):
@@ -48,7 +48,7 @@ def sum_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["numerical_reducers"]
-    out.promote_value_type(x, func = "sum", manual = manual)
+    out.sq.promote_value_type(x, func = "sum", manual = manual)
   return out
 
 def variance_(x, dimension, track_types = False, **kwargs):
@@ -57,7 +57,7 @@ def variance_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["numerical_reducers"]
-    out.promote_value_type(x, func = "variance", manual = manual)
+    out.sq.promote_value_type(x, func = "variance", manual = manual)
   return out
 
 #
@@ -71,7 +71,7 @@ def all_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["boolean_reducers"]
-    out.promote_value_type(x, func = "all", manual = manual)
+    out.sq.promote_value_type(x, func = "all", manual = manual)
   return out
 
 def any_(x, dimension, track_types = False, **kwargs):
@@ -80,7 +80,7 @@ def any_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["boolean_reducers"]
-    out.promote_value_type(x, func = "any", manual = manual)
+    out.sq.promote_value_type(x, func = "any", manual = manual)
   return out
 
 #
@@ -94,7 +94,7 @@ def count_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["count_reducers"]
-    out.promote_value_type(x, func = "count", manual = manual)
+    out.sq.promote_value_type(x, func = "count", manual = manual)
   return out
 
 def percentage_(x, dimension, track_types = False, **kwargs):
@@ -106,7 +106,7 @@ def percentage_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["count_reducers"]
-    out.promote_value_type(x, func = "percentage", manual = manual)
+    out.sq.promote_value_type(x, func = "percentage", manual = manual)
   return out
 
 #
@@ -119,7 +119,7 @@ def max_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["ordered_reducers"]
-    out.promote_value_type(x, func = "max", manual = manual)
+    out.sq.promote_value_type(x, func = "max", manual = manual)
   return out
 
 def median_(x, dimension, track_types = False, **kwargs):
@@ -128,7 +128,7 @@ def median_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["ordered_reducers"]
-    out.promote_value_type(x, func = "median", manual = manual)
+    out.sq.promote_value_type(x, func = "median", manual = manual)
   return out
 
 def min_(x, dimension, track_types = False, **kwargs):
@@ -137,7 +137,7 @@ def min_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["ordered_reducers"]
-    out.promote_value_type(x, func = "min", manual = manual)
+    out.sq.promote_value_type(x, func = "min", manual = manual)
   return out
 
 #
@@ -152,7 +152,7 @@ def first_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["universal_reducers"]
-    out.promote_value_type(x, func = "first", manual = manual)
+    out.sq.promote_value_type(x, func = "first", manual = manual)
   return out
 
 def last_(x, dimension, track_types = False, **kwargs):
@@ -161,7 +161,7 @@ def last_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["universal_reducers"]
-    out.promote_value_type(x, func = "last", manual = manual)
+    out.sq.promote_value_type(x, func = "last", manual = manual)
   return out
 
 def mode_(x, dimension, track_types = False, **kwargs):
@@ -171,5 +171,5 @@ def mode_(x, dimension, track_types = False, **kwargs):
   out = x.reduce(f, dim = dimension, **kwargs)
   if track_types:
     manual = TYPE_PROMOTION_TEMPLATES["universal_reducers"]
-    out.promote_value_type(x, func = "mode", manual = manual)
+    out.sq.promote_value_type(x, func = "mode", manual = manual)
   return out
