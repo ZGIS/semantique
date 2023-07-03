fix: Use nanosecond precision for all datetime values. Refs #28 :wrench:

semantique/datacube.py
@@ -488,7 +488,7 @@ class GeotiffArchive(Datacube):
     like = extent.sq.tz_convert(self.tz).sq.rename_dims(names)
     # Subset temporally.
     bounds = like["band"].values
-    times = [np.datetime64(x) for x in metadata["times"]]
+    times = [np.datetime64(x, "ns") for x in metadata["times"]]
     keep = [x >= bounds[0] and x <= bounds[1] for x in times]
     data = data.sel({"band": keep})
     data = data.assign_coords({"band": [x for x, y in zip(times, keep) if y]})

semantique/extent.py
@@ -338,7 +338,7 @@ class TemporalExtent(dict):
     if tz is None:
       tz = self.tz
     range_obj = range_obj.tz_convert(tz)
-    range_obj = [np.datetime64(x) for x in range_obj.tz_localize(None)]
+    range_obj = [np.datetime64(x, "ns") for x in range_obj.tz_localize(None)]
     array_obj = xr.DataArray(range_obj, dims = ["time"], coords = [range_obj])
     array_obj.sq.value_type = "datetime"
     array_obj = array_obj.sq.write_tz(tz)

semantique/processor/core.py
@@ -948,7 +948,7 @@ class QueryProcessor():
       :obj:`xarray.DataArray`
 
     """
-    dt = np.datetime64(block["content"]["start"])
+    dt = np.datetime64(block["content"]["start"], "ns")
     tz = pytz.timezone(block["content"]["tz"])
     if tz != self.tz.zone:
       dt = utils.convert_datetime64(dt, tz, self.tz)
@@ -969,8 +969,8 @@ class QueryProcessor():
       :obj:`xarray.DataArray`
 
     """
-    start = np.datetime64(block["content"]["start"])
-    end = np.datetime64(block["content"]["end"])
+    start = np.datetime64(block["content"]["start"], "ns")
+    end = np.datetime64(block["content"]["end"], "ns")
     tz = pytz.timezone(block["content"]["tz"])
     if tz != self.tz:
       start = utils.convert_datetime64(start, tz, self.tz)

semantique/processor/utils.py
@@ -122,7 +122,7 @@ def convert_datetime64(obj, tz_from, tz_to, **kwargs):
 
   """
   obj_new = pd.Timestamp(obj).tz_localize(tz_from).tz_convert(tz_to, **kwargs)
-  return np.datetime64(obj_new.tz_localize(None))
+  return np.datetime64(obj_new.tz_localize(None), "ns")
 
 def parse_extent(spatial_extent, temporal_extent, spatial_resolution,
                  temporal_resolution = None, crs = None, tz = None, trim = True):