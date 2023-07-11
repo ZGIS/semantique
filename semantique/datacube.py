0ec719ffde5dc3f9f1b16d3fd9b1adfccc656e7d
Luuk van der Meer <luukvandermeer@live.nl>
2023-07-11 15:51:15 +0200

    fix: Revert previous changes because of unwanted side effects. Refs #31 :wrench:

--- semantique/datacube.py
+++ semantique/datacube.py
@@ -278,7 +278,7 @@ class Opendatacube(Datacube):
     # Format loaded data.
     data = self._format(data, metadata, extent)
     # Mask invalid data.
-    data = self._mask(data, metadata)
+    data = self._mask(data)
     # PROVISIONAL FIX: Convert value type to float.
     # Sentinel-2 data may be loaded as unsigned integers.
     # This gives problems e.g. with divisions that return negative values.
@@ -355,26 +355,12 @@ class Opendatacube(Datacube):
     data["spatial_feats"].sq.value_labels = extent["spatial_feats"].sq.value_labels
     return data
 
-  def _mask(self, data, metadata):
+  def _mask(self, data):
     # Step I: Mask nodata values.
     data = masking.mask_invalid_data(data)
     # Step II: Mask values outside of the spatial extent.
     # This is needed since data are initially loaded for the bbox of the extent.
     data = data.where(data["spatial_feats"].notnull())
-    # Step III: Mask values outside the specified value range.
-    # This value range is specified in the metadata object.
-    # For categorical data it contains all possible category indices.
-    # For numerical data it contains an lower and an upper bound.
-    if isinstance(metadata["values"], list):
-      indices = [x["id"] for x in metadata["values"]]
-      data = data.where(data.isin(indices))
-    else:
-      lower = metadata["values"]["min"]
-      upper = metadata["values"]["max"]
-      if lower is not None:
-        data = data.where(data >= lower)
-      if upper is not None:
-        data = data.where(data <= upper)
     return data
 
 class GeotiffArchive(Datacube):
@@ -559,7 +545,7 @@ class GeotiffArchive(Datacube):
     # Format the loaded data.
     data = self._format(data, metadata, extent)
     # Mask invalid data.
-    data = self._mask(data, metadata)
+    data = self._mask(data)
     # PROVISIONAL FIX: Convert value type to float.
     # Sentinel-2 data may be loaded as unsigned integers.
     # This gives problems e.g. with divisions that return negative values.
@@ -636,24 +622,10 @@ class GeotiffArchive(Datacube):
     data.name = os.path.splitext(metadata["file"])[0]
     return data
 
-  def _mask(self, data, metadata):
+  def _mask(self, data):
     # Step I: Mask nodata values.
     data = data.where(data != data.rio.nodata)
     # Step II: Mask values outside of the spatial extent.
     # This is needed since data are initially loaded for the bbox of the extent.
     data = data.where(data["spatial_feats"].notnull())
-    # Step III: Mask values outside the specified value range.
-    # This value range is specified in the metadata object.
-    # For categorical data it contains all possible category indices.
-    # For numerical data it contains an lower and an upper bound.
-    if isinstance(metadata["values"], list):
-      indices = [x["id"] for x in metadata["values"]]
-      data = data.where(data.isin(indices))
-    else:
-      lower = metadata["values"]["min"]
-      upper = metadata["values"]["max"]
-      if lower is not None:
-        data = data.where(data >= lower)
-      if upper is not None:
-        data = data.where(data <= upper)
     return data
