cbb9a6df7592a6cc4042b5f877483d5e168a70eb
Felix Kröber <felixkroeber@web.de>
2024-09-17 11:49:52 +0200

    feat: semantic pre-evaluation of temporal filter ops :gift:

--- semantique/processor/core.py
+++ semantique/processor/core.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
 import numpy as np
+import pandas as pd
 import copy
 import inspect
 import logging
@@ -8,7 +9,9 @@ import pytz
 import warnings
 import xarray as xr
 
+from datetime import datetime
 from semantique import exceptions
+from semantique.dimensions import TIME, SPACE, X, Y
 from semantique.processor import arrays, operators, reducers, values, utils
 
 logger = logging.getLogger(__name__)
@@ -67,9 +70,9 @@ class QueryProcessor():
     self.custom_reducers = custom_reducers
     self.preview = preview
     if cache is None:
-        self.cache = Cache()
+      self.cache = Cache()
     else:
-        self.cache = cache
+      self.cache = cache
 
   @property
   def response(self):
@@ -1331,6 +1334,433 @@ class FakeProcessor(QueryProcessor):
     self._cache.build(block["reference"])
     return xr.full_like(self._extent, np.nan)
 
+class FilterProcessor(QueryProcessor):
+  """
+  Worker that processes a semantic query recipe to retrieve the required temporal
+  extent of all layers that are used in the recipe.
+
+  Parameters
+  ----------
+    recipe : QueryRecipe
+      The query recipe to be processed.
+    datacube : Datacube
+      The datacube instance to process the query against.
+    mapping : Mapping
+      The mapping instance to process the query against.
+    extent : :obj:`xarray.DataArray`
+      The spatio-temporal extent in which the query should be processed. Should
+      be given as an array with a temporal dimension and two spatial dimensions
+      such as returned by
+      :func:`parse_extent <semantique.processor.utils.parse_extent>`.
+    custom_verbs : :obj:`dict`, optional
+      User-defined verbs that may be used when executing the query recipe in
+      addition to the built-in verbs in semantique.
+    custom_operators : :obj:`dict`, optional
+      User-defined operator functions that may be used when evaluating
+      expressions with the evaluate verb in addition to the built-in operators
+      in semantique. Built-in operators with the same name will be overwritten.
+    custom_reducers : :obj:`dict`, optional
+      User-defined reducer functions that may be used when reducing array
+      dimensions with the reduce verb in addition to the built-in reducers in
+      semantique. Built-in reducers with the same name will be overwritten.
+    track_types : :obj:`bool`
+      Should the query processor keep track of the value type of arrays
+      when applying processes, and promote them if necessary? This option is
+      always disabled for the FakeProcessor since it doesn't evaualte processes
+      and therefore can't check the validity of the types of the arrays.
+    preview : :obj:`bool`
+      Run the query processor with reduced resolution to test the recipe execution.
+      Preview-runs are necessary if cache should be used.
+    cache : :obj:`Cache`
+      The cache object that is used to store data layers.
+    watch_layer : :obj:`str`
+      The layer that is currently being focussed on. All temporal filter operations
+      are analyzed regarding the effect they are having on this layer.
+    meta_timestamps : :obj:`DatetimeIndex` or :obj:`Series`
+      The timestamps that are used as a starting point to determine the final
+      temporal extent. The timestamps will be filtered by the temporal filters.
+      If not provided, a default range from 1960 to the current time in hourly
+      intervals is used to evaluate the temporal filters.
+  """
+  def __init__(self, recipe, datacube, mapping, extent, custom_verbs = None,
+               custom_operators = None, custom_reducers = None, track_types = True,
+               preview = False, cache = None, watch_layer = None, meta_timestamps = None):
+    super(FilterProcessor, self).__init__(
+      recipe, datacube, mapping, extent, custom_verbs=custom_verbs,
+      custom_operators=custom_operators, custom_reducers=custom_reducers,
+      track_types=track_types, preview=preview, cache=cache
+      )
+    self.track_types = False
+    self.watch_layer = watch_layer
+    if meta_timestamps is not None:
+      self.meta_timestamps = meta_timestamps
+    else:
+      self.meta_timestamps = pd.date_range(
+        start = '1960-01-01',
+        end = datetime.now(),
+        freq = 'h'
+      )
+    self.meta_timestamps = pd.to_datetime(self.meta_timestamps).sort_values()
+
+  @property
+  def crs(self):
+    """:obj:`pyproj.crs.CRS`: Spatial coordinate reference system in which the
+    query should be processed."""
+    return self._extent.sqm.crs
+
+  @property
+  def spatial_resolution(self):
+    """:obj:`list`: Spatial resolution in which the query should be
+    processed."""
+    return self._extent.sqm.spatial_resolution
+
+  @property
+  def tz(self):
+    """:obj:`datetime.tzinfo`: Time zone in which the query should be
+    processed."""
+    return self._extent.sqm.tz
+
+  @property
+  def watch_layer(self):
+    """:obj:`str`: The layer that is currently being focussed on."""
+    return self._watch_layer
+
+  @watch_layer.setter
+  def watch_layer(self, value):
+    self._watch_layer = value
+
+  @property
+  def meta_timestamps(self):
+    """:obj:`DatetimeIndex` or :obj:`Series`: The timestamps that are used as
+    a starting point to determine the final temporal extent."""
+    return self._meta_timestamps
+
+  @meta_timestamps.setter
+  def meta_timestamps(self, value):
+    self._meta_timestamps = value
+
+  def execute(self):
+    """Execute a semantic query.
+
+    During query execution, the query processor executes the result
+    instructions of the query recipe. It solves all references, evaluates them
+    into arrays, and applies the defined actions to them.
+
+    Returns
+    -------
+      :obj:`FilterProcessor`
+        An updated filter processor instance, with a :attr:`response` property
+        containing the resulting timestamps that are processed in the recipe.
+
+    """
+    logger.info("Started executing the semantic query")
+    # Run fake processor to get a dict of all layers to init response object
+    self.fp = FakeProcessor(
+      recipe=self.recipe,
+      datacube=self.datacube,
+      mapping=self.mapping,
+      extent=self.extent,
+      track_types=self.track_types,
+      custom_verbs=self.custom_verbs,
+      custom_operators=self.custom_operators,
+      custom_reducers=self.custom_reducers,
+      preview=self.preview,
+      cache=self.cache
+    )
+    _ = self.fp.optimize().execute()
+    lyrs = [list(x) for x in set(tuple(x) for x in self.fp.cache.seq)]
+    self._response = {"_".join(x): {} for x in lyrs}
+    if self._contains_filter(self.recipe):
+      # Execute instructions for each layer & result in the recipe.
+      for lyr in lyrs:
+        self.watch_layer = "_".join(lyr)
+        logger.info(f"Evaluate temporal filter for layer: '{lyr}'")
+        for x in self._recipe:
+          logger.info(f"Started executing result: '{x}'")
+          result = self.call_handler(self._recipe[x])
+          result.name = x
+          self._response[self.watch_layer][x] = result
+          logger.info(f"Finished executing result: '{x}'")
+      logger.info("Finished executing the semantic query")
+      # Omit non-active results.
+      for lyr,arr_dict in self._response.items():
+        self._response[lyr] = {
+          k: v for k, v in arr_dict.items() if v.sqm.active
+        }
+      # Combine temporal extents of Collections.
+      for lyr,arr_dict in self._response.items():
+        for k,v in arr_dict.items():
+          if type(v.sqm).__name__ == 'MetaCollection':
+            self._response[lyr][k] = v.sqm.merge(reducers.any_, track_types=False)
+      # Retrieve valid temporal indices per layer and result.
+      # Valid indices are those that are not null.
+      for lyr,arr_dict in self._response.items():
+        for res,arr in arr_dict.items():
+          # Create empty response obj.
+          out = []
+          # Reduce arrays to at most 3 dimensions.
+          if arr.ndim > 3:
+            dim = [x for x in arr.dims if x not in ["time", "x", "y"]]
+            assert len(dim) == 1, "Only one dimension can be reduced"
+            arr = arr.sqm.reduce(reducers.any_, dim[0], track_types=False)
+          # Extract temporal indices from array itself.
+          if "time" in arr.dims:
+            reduce_dims = [x for x in arr.dims if x != "time"]
+            out.append(arr.time[arr.isnull().sum(reduce_dims) == 0])
+          # Extract saved results from vault.
+          if arr.sqm.vault is not None:
+            out.append(arr.sqm.vault)
+          # Combine results from array and vault.
+          if len(out) > 1:
+            out = xr.DataArray(np.unique(np.concatenate(out)), dims="time", name="time")
+          else:
+            out = out[0]
+          self._response[lyr][res] = out
+      # Create temporal extents' union over results for each layer.
+      for lyr,arr_dict in self._response.items():
+        time_coords = [arr_dict[key].values for key in arr_dict]
+        merged_time = np.unique(np.concatenate(time_coords))
+        merged_time.sort()
+        self._response[lyr] = pd.to_datetime(merged_time)
+    else:
+      # Shortcut if no temporal filter is present.
+      for lyr in lyrs:
+        self._response["_".join(lyr)] = pd.to_datetime(self.meta_timestamps)
+    # Return result.
+    self._response = {k: v for k, v in sorted(self._response.items())}
+    out = self._response
+    logger.debug(f"Responding:\n{out}")
+    return out
+
+  def call_verb(self, name, params):
+    """Apply a verb to the active evaluation object.
+
+    Parameters
+    -----------
+      name : :obj:`str`
+        Name of the verb.
+      params : :obj:`dict`
+        Parameters to be forwarded to the verb.
+
+    Returns
+    -------
+      :obj:`xarray.DataArray` or
+      :obj:`MetaCollection <processor.arrays.MetaCollection>`
+
+    """
+    # Get the object to apply the verb to.
+    obj = self._get_eval_obj()
+    # Apply the verb.
+    verb = getattr(obj.sqm, name)
+    out = verb(**params)
+    # Warn when output array is empty.
+    try:
+      is_empty = out.sqm.is_empty
+    except AttributeError:
+      is_empty = out.is_empty
+    if is_empty:
+      warnings.warn(
+        f"Verb '{name}' returned an empty array"
+      )
+    logger.debug(f"Applied verb {name}:\n{out}")
+    return out
+
+
+  def handle_concept(self, block):
+    """Handler for semantic concept references.
+
+    Parameters
+    ----------
+      block : :obj:`dict`
+        Textual representation of a building block of type "concept".
+
+    Returns
+    -------
+      :obj:`xarray.DataArray`
+
+    """
+    logger.debug(f"Translating concept {block['reference']}")
+    out = self._mapping.translate(
+      *block["reference"],
+      property = block["property"] if "property" in block else None,
+      extent = self._extent,
+      datacube = self._datacube,
+      eval_obj = self._get_eval_obj(),
+      processor = FilterProcessor,
+      preview = self._preview,
+      cache = self._cache,
+      custom_verbs = self._custom_verbs,
+      custom_operators = self._custom_operators,
+      custom_reducers = self._custom_reducers,
+      track_types = self._track_types,
+      watch_layer = self._watch_layer,
+      meta_timestamps = self._meta_timestamps,
+    )
+    logger.debug(f"Translated concept {block['reference']}:\n{out}")
+    return out
+
+  def handle_collection(self, block):
+    """Handler for collection references.
+
+    Parameters
+    ----------
+      block : :obj:`dict`
+        Textual representation of a building block of type "collection".
+
+    Returns
+    -------
+      :obj:`processor.arrays.MetaCollection`
+
+    """
+    logger.debug("Constructing collection of arrays")
+    list_obj = [self.call_handler(x) for x in block["elements"]]
+    out = arrays.MetaCollection(list_obj)
+    logger.debug(f"Constructed collection of:\n{[x.name for x in out]}")
+    return out
+
+  def handle_label(self, block):
+    """Handler for value labels.
+
+    Parameters
+    ----------
+      block : :obj:`dict`
+        Textual representation of a building block of type "label".
+
+    Returns
+    -------
+      :obj:None
+    """
+    return None
+
+  def handle_layer(self, block):
+    """Handler for data layer references.
+
+    Parameters
+    ----------
+      block : :obj:`dict`
+        Textual representation of a building block of type "layer".
+
+    Returns
+    -------
+      :obj:`xarray.DataArray`
+    """
+    self._cache.build(block["reference"])
+    # Create a data array with the same shape as temporal extent.
+    x_coords = [0, 1]
+    y_coords = [0, 1]
+    data = np.ones((len(self.meta_timestamps), len(y_coords), len(x_coords)))
+    data_array = xr.DataArray(
+      data,
+      coords = {
+        TIME: self.meta_timestamps,
+        X: y_coords,
+        Y: x_coords,
+        "spatial_feats": ((Y, X), np.ones((len(y_coords), len(x_coords))))
+      },
+      dims = [TIME, Y, X]
+    )
+    # Set relevant data array properties.
+    layer_key = "_".join(block["reference"])
+    data_array.name = layer_key
+    if layer_key == self._watch_layer:
+      data_array.sqm.active = True
+    else:
+      data_array.sqm.active = False
+    data_array.sqm.locked = False
+    return data_array
+
+  def handle_result(self, block):
+    """Handler for result references.
+
+    Parameters
+    ----------
+      block : :obj:`dict`
+        Textual representation of a building block of type "result".
+
+    Returns
+    -------
+      :obj:`xarray.DataArray` or
+      :obj:`MetaCollection <semantique.processor.arrays.MetaCollection>`
+
+    Raises
+    ------
+      :obj:`exceptions.UnknownResultError`
+        If the referenced result is not present in the query recipe.
+
+    """
+    name = block["name"]
+    logger.debug(f"Fetching result '{name}'")
+    # Process referenced result if it is not processed yet.
+    if name not in self._response[self.watch_layer]:
+      try:
+        instructions = self._recipe[name]
+      except KeyError:
+        raise exceptions.UnknownResultError(
+          f"Recipe does not contain result '{name}'"
+        )
+      logger.info(f"Started executing result: '{name}'")
+      result = self.call_handler(instructions)
+      result.name = name
+      self._response[self.watch_layer][name] = result
+      logger.info(f"Finished executing result: '{name}'")
+    # Return referenced result.
+    out = self._response[self.watch_layer][name]
+    logger.debug(f"Fetched result '{name}':\n{out}")
+    return out
+
+  def _contains_filter(self, params):
+    """Recursively check for temporal filter in nested dictionaries or lists.
+
+    Parameters
+    ----------
+      params : :obj:`dict` or :obj:`list`
+        The parameters to be checked.
+
+    Returns
+    -------
+      :obj:`bool`
+        True if temporal filter is found in the parameters, False otherwise.
+    """
+    if isinstance(params, dict):
+      if params.get('type') == 'verb' and params.get('name') == 'filter':
+        if 'params' in params:
+          return self._contains_extract_time(params['params'])
+      for key, value in params.items():
+        if isinstance(value, (dict, list)):
+          if self._contains_filter(value):
+            return True
+    elif isinstance(params, list):
+      for item in params:
+        if self._contains_filter(item):
+          return True
+    return False
+
+  def _contains_extract_time(self, params):
+    """Recursively check for 'extract time' in nested dictionaries or lists.
+
+    Parameters
+    ----------
+      params : :obj:`dict` or :obj:`list`
+        The parameters to be checked.
+
+    Returns
+    -------
+      :obj:`bool`
+        True if 'extract time' is found in the parameters, False otherwise.
+    """
+    if isinstance(params, dict):
+      if params.get('type') == 'verb' and params.get('name') == 'extract':
+        if 'params' in params and params['params'].get('dimension') == 'time':
+          return True
+      for key, value in params.items():
+        if isinstance(value, (dict, list)):
+          if self._contains_extract_time(value):
+            return True
+    elif isinstance(params, list):
+      for item in params:
+        if self._contains_extract_time(item):
+          return True
+    return False
 
 class Cache:
   """Cache of retrieved data layers.
--- semantique/recipe.py
+++ semantique/recipe.py
@@ -1,113 +1,153 @@
-from semantique.processor.core import QueryProcessor, FakeProcessor
+import semantique as sq
+from semantique.processor.core import QueryProcessor, FakeProcessor, FilterProcessor
 from semantique.visualiser.visualise import show
 
+
 class QueryRecipe(dict):
-  """Dict-like container to store instructions of a query recipe.
-
-  Parameters
-  ----------
-    results : :obj:`dict` of :obj:`ArrayProxy`
-      Dictionary containing result names as keys and result instructions as
-      values. If :obj:`None`, an empty recipe is constructed.
-
-  Returns
-  -------
-    :obj:`dict` of :obj:`ArrayProxy`:
-      The query recipe as a dictionary containing result names as keys and
-      result instructions as values.
-
-  Examples
-  --------
-  >>> import semantique as sq
-  >>> recipe = sq.QueryRecipe()
-  >>> recipe["map"] = sq.entity("water").reduce("time", "count")
-  >>> recipe["series"] = sq.entity("water").reduce("space", "count")
-
-  """
-  def __init__(self, results = None):
-    obj = {} if results is None else results
-    super(QueryRecipe, self).__init__(obj)
-
-  def execute(self, datacube, mapping, space, time, run_preview = False,
-              cache_data = True, **config):
-    """Execute a query recipe.
-
-    This function initializes a :obj:`processor.core.QueryProcessor` instance
-    and uses it to process the query. It runs through all distinct phases of
-    query processing: parsing, optimization and execution.
+    """Dict-like container to store instructions of a query recipe.
 
     Parameters
     ----------
-      datacube : Datacube
-        The datacube instance to process the query against.
-      mapping : Mapping
-        The mapping instance to process the query against.
-      space : SpatialExtent
-        The spatial extent in which the query should be processed.
-      time : TemporalExtent
-        The temporal extent in which the query should be processed.
-      run_preview : :obj:`bool`
-        Should a preview run with reduced spatial resolution be performed?
-        A preview run enables to test if the recipe execution succeeds
-        and allows to inspect the results.
-      cache_data : :obj:`bool`
-        Should the query processor cache the data references as provided by the
-        mapped concepts? Enabling caching increases the memory footprint while
-        reducing the I/O time to retrieve data if the same data layer is
-        referenced multiple times.
-      **config:
-        Additional configuration parameters forwarded to
-        :func:`QueryProcessor.parse <processor.core.QueryProcessor.parse>`.
+      results : :obj:`dict` of :obj:`ArrayProxy`
+        Dictionary containing result names as keys and result instructions as
+        values. If :obj:`None`, an empty recipe is constructed.
 
     Returns
     -------
-      :obj:`dict` of :obj:`xarray.DataArray`:
-        The response of the query processor as a dictionary containing result
-        names as keys and result arrays as values.
+      :obj:`dict` of :obj:`ArrayProxy`:
+        The query recipe as a dictionary containing result names as keys and
+        result instructions as values.
 
     Examples
     --------
     >>> import semantique as sq
-    >>> import geopandas as pd
-
     >>> recipe = sq.QueryRecipe()
     >>> recipe["map"] = sq.entity("water").reduce("time", "count")
     >>> recipe["series"] = sq.entity("water").reduce("space", "count")
 
-    >>> dc = sq.datacube.GeotiffArchive("files/layout_gtiff.json", src = "layers_gtiff.zip")
-    >>> mapping = sq.mapping.Semantique("files/mapping.json")
-    >>> space = sq.SpatialExtent(gpd.read_file("files/footprint.geojson"))
-    >>> time = sq.TemporalExtent("2019-01-01", "2020-12-31")
-    >>> config = {"crs": 3035, "tz": "UTC", "spatial_resolution": [-1800, 1800]}
-
-    >>> recipe.execute(dc, mapping, space, time, **config)
-
     """
-    if cache_data:
-      fp = FakeProcessor.parse(self, datacube, mapping, space, time, **config)
-      _ = fp.optimize().execute()
-      cache = fp.cache
-    else:
-      cache = None
 
-    qp = QueryProcessor.parse(
-      self,
-      datacube,
-      mapping,
-      space,
-      time,
-      preview=run_preview,
-      cache=cache,
-      **config
-    )
-    return qp.optimize().execute()
+    def __init__(self, results=None):
+        obj = {} if results is None else results
+        super(QueryRecipe, self).__init__(obj)
 
-  def visualise(self):
-    """Visualise the recipe in a web browser.
+        def execute(
+            self,
+            datacube,
+            mapping,
+            space,
+            time,
+            filter_check=True,
+            run_preview=False,
+            cache_data=True,
+            **config
+        ):
+            """Execute a query recipe.
 
-    This method visualises the recipe in a web browser. 
-    The visualisation is based on Blockly, a web-based visual programming
-    editor. The recipe is converted into Blockly XML format and served
-    to the browser.
-    """
-    show(self)
+            This function initializes a :obj:`processor.core.QueryProcessor` instance
+            and uses it to process the query. It runs through all distinct phases of
+            query processing: parsing, optimization and execution.
+
+                Parameters
+                ----------
+                  datacube : Datacube
+                    The datacube instance to process the query against.
+                  mapping : Mapping
+                    The mapping instance to process the query against.
+                  space : SpatialExtent
+                    The spatial extent in which the query should be processed.
+                  time : TemporalExtent
+                    The temporal extent in which the query should be processed.
+                  filter_check : :obj:`bool`
+                    Should the query processor evaluate possible temporal filter operations
+                    upfront? This can reduce the amount of data to be processed in the
+                    subsequent evaluation process. If the recipe doesn't contain any
+                    temporal filter operations, this flag has no effect.
+                  run_preview : :obj:`bool`
+                    Should a preview run with reduced spatial resolution be performed?
+                    A preview run enables to test if the recipe execution succeeds
+                    and allows to inspect the results.
+                  cache_data : :obj:`bool`
+                    Should the query processor cache the data references as provided by the
+                    mapped concepts? Enabling caching increases the memory footprint while
+                    reducing the I/O time to retrieve data if the same data layer is
+                    referenced multiple times.
+                  **config:
+                    Additional configuration parameters forwarded to
+                    :func:`QueryProcessor.parse <processor.core.QueryProcessor.parse>`.
+
+            Returns
+            -------
+              :obj:`dict` of :obj:`xarray.DataArray`:
+                The response of the query processor as a dictionary containing result
+                names as keys and result arrays as values.
+
+            Examples
+            --------
+            >>> import semantique as sq
+            >>> import geopandas as pd
+
+            >>> recipe = sq.QueryRecipe()
+            >>> recipe["map"] = sq.entity("water").reduce("time", "count")
+            >>> recipe["series"] = sq.entity("water").reduce("space", "count")
+
+            >>> dc = sq.datacube.GeotiffArchive("files/layout_gtiff.json", src = "layers_gtiff.zip")
+            >>> mapping = sq.mapping.Semantique("files/mapping.json")
+            >>> space = sq.SpatialExtent(gpd.read_file("files/footprint.geojson"))
+            >>> time = sq.TemporalExtent("2019-01-01", "2020-12-31")
+            >>> config = {"crs": 3035, "tz": "UTC", "spatial_resolution": [-1800, 1800]}
+
+            >>> recipe.execute(dc, mapping, space, time, **config)
+
+            """
+            # Pre-evaluation
+            # For GeotiffArchive datacubes, subsetting data is not feasible
+            if type(datacube) == sq.datacube.GeotiffArchive:
+                filter_check = False
+
+            if filter_check:
+                # Tbd: Add meta_timestamps as input to FilterProcessor, retrieved from datacube
+                # For STACCube, meta_timestamps can be retrieved from src
+                # For OpenDataCube, tbd
+                # Retrieve required timestamps according to filter operations
+                fp = FilterProcessor.parse(
+                    self, datacube, mapping, space, time, **config
+                )
+                valid_timestamps = fp.optimize().execute
+                if cache_data:
+                    # FilterProcessor contains a FakeProcessor instance
+                    cache = fp.fp.cache
+                else:
+                    cache = None
+            else:
+                if cache_data:
+                    fp = FakeProcessor.parse(
+                        self, datacube, mapping, space, time, **config
+                    )
+                    _ = fp.optimize().execute()
+                    cache = fp.cache
+                else:
+                    cache = None
+
+            # Evaluation
+            qp = QueryProcessor.parse(
+                self,
+                datacube,
+                mapping,
+                space,
+                time,
+                preview=run_preview,
+                cache=cache,
+                **config
+            )
+            return qp.optimize().execute()
+
+    def visualise(self):
+        """Visualise the recipe in a web browser.
+
+        This method visualises the recipe in a web browser.
+        The visualisation is based on Blockly, a web-based visual programming
+        editor. The recipe is converted into Blockly XML format and served
+        to the browser.
+        """
+        show(self)
